0c04333439d19b93d9a384e331427db2fae1df47
Nareshmalviya09 <Nareshmalviya4321@gmail.com>
2025-08-14 10:12:14 +0530

    new programs add

--- JAVA--1/dscArray.java
+++ JAVA--1/dscArray.java
@@ -1,18 +1,20 @@
-class secondhighest2{
+class dscArray{
     public static void main(String[] args) {
         int arr[]={12,30,87,97,890,825,900};
         int temp=0;
-        for(int i=0;i<arr.length;i++){ 
-        for(int j=0;j<arr.length-1;j++) 
+        for(int i=0;i<arr.length;i++){  //0<5
+        for(int j=0;j<arr.length-1;j++) // 0<5-1
         {
-            if (arr[j]>arr[j+1]){    
+            if (arr[j]<arr[j+1]){    
                 temp=arr[j];         
                 arr[j]=arr[j+1];      
                 arr[j+1]=temp;       
             }
          }
            }
-           int max2=arr[arr.length-2];
-              System.out.println(max2);
+           for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i] +" ");
+           }
+             
     }
 }
---
+++ JAVA--1/nthlargest.java
@@ -0,0 +1,20 @@
+class nthlargest{
+    public static void main(String[] args) {
+        int arr[]={12,30,87,97,890,825,900};
+        int temp=0;
+        int n=3;
+        for(int i=0;i<n;i++){  //0<5
+        for(int j=i+1;j<arr.length;++j) // 0<5-1
+        {
+            if (arr[i]<arr[j]){    
+                temp=arr[i];         
+                arr[i]=arr[j];      
+                arr[j]=temp;       
+            }
+         }
+           }          
+            System.out.print(arr[n-1]);
+            
+             
+    }
+}
